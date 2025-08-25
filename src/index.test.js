58cd91454c5aae48417b1049aa0d03d26af06d44
Frazer Smith <frazer.dev@icloud.com>
2025-08-25 12:17:41 +0100

    test(index): add testcontext jsdoc to satisfy ts2775 on assertions

--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,8 @@
 const { describe, it } = require("node:test");
 const { fixLatin1ToUtf8, REPLACEMENTS } = require("./index");
 
+/** @typedef {import('node:test').TestContext} TestContext */
+
 describe("fixLatin1ToUtf8 function", () => {
 	const entries = Object.entries(REPLACEMENTS);
 	const entriesLength = entries.length;
@@ -11,25 +13,26 @@ describe("fixLatin1ToUtf8 function", () => {
 		// Destructuring adds overhead, so use index access
 		const actual = entries[i][0];
 		const expected = entries[i][1];
-		it(`Replaces ${actual} with ${expected}`, (t) => {
+		it(`Replaces ${actual} with ${expected}`, (/** @type {TestContext} */ t) => {
 			t.plan(1);
 			t.assert.strictEqual(fixLatin1ToUtf8(actual), expected);
 		});
 	}
 
-	it("Replaces multiple characters", (t) => {
+	it("Replaces multiple characters", (/** @type {TestContext} */ t) => {
 		t.plan(1);
 		t.assert.strictEqual(fixLatin1ToUtf8("â€šÆ’â€žâ€¦â€\u00A0"), "‚ƒ„…†");
 	});
 
-	it("Does not alter a string without Latin-1 characters", (t) => {
+	it("Does not alter a string without Latin-1 characters", (/** @type {TestContext} */ t) => {
 		t.plan(1);
 		const str = "Hello, world!";
 		t.assert.strictEqual(fixLatin1ToUtf8(str), str);
 	});
 
-	it("Throws an error if the argument is not a string", (t) => {
+	it("Throws an error if the argument is not a string", (/** @type {TestContext} */ t) => {
 		t.plan(1);
+		// @ts-expect-error
 		t.assert.throws(() => fixLatin1ToUtf8(123), TypeError);
 	});
 });
